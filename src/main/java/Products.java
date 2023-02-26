6678a82246a0cbd4c64b558b4af27a628a7e708f
GeorgeMoroz1 <yura0991@mail.ru>
2023-02-26 14:16:45 +0300

    Initial commit

---
+++ src/main/java/Products.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Products {
+
+    private static HashMap<String, Integer> products = new HashMap<>();
+
+    static {
+        products.put("Хлеб", 50);
+        products.put("Масло", 150);
+        products.put("Колбаса", 300);
+        products.put("Пирожок", 50);
+    }
+
+    public static HashMap<String, Integer> getProducts() {
+        return products;
+    }
+
+    public static void showProducts() {
+        System.out.println("В МАГАЗИНЕ В НАЛИЧИИ");
+        for (Map.Entry<String, Integer> productAndPrice : products.entrySet()) {
+            System.out.println(productAndPrice.getKey() + " за " + productAndPrice.getValue() + " руб./шт.");
+        }
+    }
+}
